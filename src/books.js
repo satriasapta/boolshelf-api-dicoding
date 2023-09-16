2b22435b50ab106f8d64e70626a36884fba08898
Satria Sapta <satriasapta48@gmail.com>
2023-09-16 22:03:23 +0700

    submission akhir  back-end pemula

---
+++ src/books.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+const books = [];
+
+module.exports = books;
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+
+const {
+    addBookHandler,
+    getAllBooksHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+];
+module.exports = routes;
